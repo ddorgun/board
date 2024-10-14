cd94a18216f816d04dbb4e1727208a80104b9f92
ddorgun <ddorgun@gmail.com>
2024-10-15 02:14:20 +0900

    인증 가드 추가

--- src/users/users-http.module.ts
+++ src/users/users-http.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users.module';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { AuthGuard } from 'src/common/guard/auth.guard';
 
 @Module({
   imports: [UsersModule],
